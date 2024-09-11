565f37efa1ee993daff81e427325308a90b8e1a7
Bekimoon0043 <bereketamare0043@gmail.com>
2024-09-12 00:33:44 +0300

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/NEWSWEBSITE/",
-  plugins: [react()],
-})
+     base: '/NEWSWEBSITE/',
+     // other config...
+});
+   
